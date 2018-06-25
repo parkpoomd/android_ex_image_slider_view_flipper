1d0c00f2a884f4d4d22915c86129ff0011a6b9b2
parkpoomd <parkpoom.d@gmail.com>
2018-06-25 21:34:57 +0700

    Image slider view flipper

--- app/src/main/java/com/example/deer/imagesliderviewflipper/MainActivity.java
+++ app/src/main/java/com/example/deer/imagesliderviewflipper/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.deer.imagesliderviewflipper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper = (ViewFlipper) findViewById(R.id.v_flipper);
+        viewFlipper.setFlipInterval(3000); // 3 second
+        viewFlipper.setAutoStart(true);
+
+        // animation
+        viewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
